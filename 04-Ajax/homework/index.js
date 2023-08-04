52f592fa0a7ff2ecec40f4053ff11fece4b42090
Jonathan Garcia <122134746+JonaG684@users.noreply.github.com>
2023-08-04 13:52:16 -0300

    HW con EMS6

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,27 +1,23 @@
 $(document).ready(function() {
   // Función para mostrar todos los amigos al hacer clic en el botón
   $("#btn").click(function() {
-    $.get("http://localhost:5000/amigos", function(data) {
+    $.get("http://localhost:5000/amigos", (data) => {
       const lista = $("#lista");
       lista.empty(); // Limpiamos la lista antes de agregar los amigos
-
-      data.forEach(function(amigo) {
-        lista.append("<li>" + amigo.name + " - Edad: " + amigo.age + " - Email: " + amigo.email + "</li>");
-      });
+      data.forEach(({ name }) => lista.append(`<li>${name}</li>`));
     });
   });
 
   // Función para buscar un amigo por su ID al hacer clic en el botón "Buscar"
   $("#search").click(function() {
     const amigoId = $("#input").val();
-
     if (amigoId) {
-      $.get("http://localhost:5000/amigos/" + amigoId, function(data) {
+      $.get(`http://localhost:5000/amigos/${amigoId}`, (data) => {
         const amigoEncontrado = $("#amigo");
         amigoEncontrado.empty(); // Limpiamos el contenido antes de mostrar el amigo
-
         if (data) {
-          amigoEncontrado.text("Amigo encontrado: " + data.name);
+          const { name } = data;
+          amigoEncontrado.text(`Amigo encontrado: ${name}`);
         } else {
           amigoEncontrado.text("No se encontró un amigo con el ID proporcionado.");
         }
@@ -32,17 +28,12 @@ $(document).ready(function() {
   // Función para borrar un amigo por su ID al hacer clic en el botón "Delete"
   $("#delete").click(function() {
     const amigoIdBorrar = $("#inputDelete").val();
-
     if (amigoIdBorrar) {
       $.ajax({
-        url: "http://localhost:5000/amigos/" + amigoIdBorrar,
+        url: `http://localhost:5000/amigos/${amigoIdBorrar}`,
         type: "DELETE",
-        success: function() {
-          $("#success").text("Amigo borrado correctamente.");
-        },
-        error: function() {
-          $("#success").text("No se pudo borrar el amigo. Verifica el ID proporcionado.");
-        }
+        success: () => $("#success").text("Amigo borrado correctamente."),
+        error: () => $("#success").text("No se pudo borrar el amigo. Verifica el ID proporcionado.")
       });
     }
   });
